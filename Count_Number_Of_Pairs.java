c4da2a8e0e88651a5f0842a727d068b1862f1ac3
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-27 01:20:16 +0530

    GFG

--- Count_Number_Of_Pairs.java
+++ Count_Number_Of_Pairs.java
@@ -20,3 +20,28 @@ class Solution {
         return cnt;
     }
 }
+
+
+//APPROACH 2 AND OPTIMIZED APPROACH
+//User function Template for Java
+//TIME COMPLEXITY:O(N)
+
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+HashMap<Integer,Integer>  hm=new HashMap<>();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]<k){
+                if(hm.containsKey(k-arr[i])){
+                    cnt+=hm.get(k-arr[i]);
+                }
+                if(hm.get(arr[i])==null){
+                    hm.put(arr[i],0);
+                }
+
+    hm.put(arr[i],hm.get(arr[i])+1);
+            }
+        }
+        return cnt;
+    }
+}
