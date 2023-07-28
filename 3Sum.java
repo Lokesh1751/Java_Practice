adee24dfbde2ad7538754d958b1d0da2b4193e25
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-28 13:08:37 +0530

    Create 3Sum.java

---
+++ 3Sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        LinkedHashSet<List<Integer>> l=new LinkedHashSet<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        if(nums.length==0){
+            return ans;
+        }
+       Arrays.sort(nums);
+       for(int i=0;i<nums.length-2;i++){
+           int j=i+1;
+           int k=nums.length-1;
+           while(j<k){
+               int sum=nums[j]+nums[k];
+               if(sum==(-nums[i])){
+                   l.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
+               }
+               else if(sum>(-nums[i])){
+                   k--;
+               }
+               else
+               {
+                   j++;
+               }
+           }
+       }
+       for(List<Integer> i:l){
+           ans.add(i);
+       }
+       return ans;
+    }
+}
