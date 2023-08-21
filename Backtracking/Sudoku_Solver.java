1ef387bba8b7377ac0e12cb4b8b9f1d168d8395a
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-08-21 15:33:17 +0530

    Create Sudoku_Solver.java

---
+++ Backtracking/Sudoku_Solver.java
@@ -0,0 +1,52 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static boolean issafe(int row,int col,int[][] board,int val){
+int n=9;
+for(int i=0;i<9;i++){
+
+	//check row
+	if(board[row][i]==val){
+		return false;
+	}
+
+	//check col
+	if(board[i][col]==val){
+		return false;
+	}
+	////3*3 matrix check
+	if(board[3*(row/3)+i/3][3*(col/3)+i%3]==val){
+		return false;
+	}
+
+
+}
+return true;
+	}
+    public static boolean solve(int[][] board){
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+				if(board[i][j]==0){
+					for(int val=1;val<=9;val++){
+						if(issafe(i,j,board,val)){
+							board[i][j]=val;
+							boolean furthursol=solve(board);
+							if(furthursol){
+								return true;
+							}
+							else{
+								board[i][j]=0;
+							}
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	public static void solveSudoku(int[][] sudoku) {
+        solve(sudoku);
+        
+	}
+}
