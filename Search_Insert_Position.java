2829865ec2c4b92230da6a332d013554977f10ea
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-08-29 00:28:45 +0530

    Create Search_Insert_Position.java

---
+++ Search_Insert_Position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<target && nums[i+1]>target){
+                return i+1;
+            }
+        }
+       if(target>nums[nums.length-1]){
+           return nums.length;
+       }
+           return 0;
+
+    }
+}
+//LEETCODE
