cf8a7203699e15d8893904a68a9a2a4a8ca9a204
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-09-05 12:49:04 +0530

    Update Merge_K_Linked_List.java

--- Linked_List/Merge_K_Linked_List.java
+++ Linked_List/Merge_K_Linked_List.java
@@ -1,3 +1,4 @@
+//APPROACH 1
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -36,4 +37,100 @@ else{
         return head;
     }
 }
+
+
+
+
+
+
+//APPROACH 2
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+         ListNode head1=list1;
+        ListNode head2=list2;
+        ListNode finalans=null;
+        ListNode anshead=null;
+        while(head1!=null && head2!=null){
+            if(head1.val>head2.val){
+                if(finalans==null){
+                    ListNode l=new ListNode(head2.val);
+                    finalans=l;
+                    anshead=l;
+                }
+                else{
+                    ListNode l=new ListNode(head2.val);
+                    finalans.next=l;
+                    finalans=l;
+                }
+                head2=head2.next;
+            }
+            else{
+                if(finalans==null){
+                    ListNode l=new ListNode(head1.val);
+                    finalans=l;
+                     anshead=l;
+                }
+                else{
+                    ListNode l=new ListNode(head1.val);
+                    finalans.next=l;
+                    finalans=l;
+                }
+                head1=head1.next;
+            }
+            
+        }
+        while(head1!=null){
+            if(finalans==null){
+                    ListNode l=new ListNode(head1.val);
+                    finalans=l;
+                     anshead=l;
+                }
+                else{
+                    ListNode l=new ListNode(head1.val);
+                    finalans.next=l;
+                    finalans=l;
+                }
+                head1=head1.next;
+        }
+         while(head2!=null){
+            if(finalans==null){
+                    ListNode l=new ListNode(head2.val);
+                    finalans=l;
+                     anshead=l;
+                }
+                else{
+                    ListNode l=new ListNode(head2.val);
+                    finalans.next=l;
+                    finalans=l;
+                }
+                head2=head2.next;
+        }
+        return anshead;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null || lists.length==0){
+            return null;
+        }
+        int interval=1;
+        while(interval<lists.length){
+            for(int i=0;i+interval<lists.length;i=i+interval*2){
+                lists[i]=mergeTwoLists(lists[i],lists[i+interval]);
+            }
+            interval=interval*2;
+        }
+        return lists[0];
+    }
+}
+
 //LEETCODE
