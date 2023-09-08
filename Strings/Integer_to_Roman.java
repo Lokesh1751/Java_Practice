47b1579ec25b4106240b19acd19b1896adf8d7f9
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-09-08 23:58:58 +0530

    Create Integer_to_Roman.java

---
+++ Strings/Integer_to_Roman.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String ans="";
+        int i=0;
+        while(num>0){
+            if(num>=values[i]){
+                ans+=romanNumerals[i];
+                num-=values[i];
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+    }
+}
+//LEETCODE
