8bc139f9e3a9905d8c45bd265a87036bd69c41e4
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-09-08 15:39:54 +0530

    Update Split_Strings_to_Make_Palindrome.java

--- Strings/Split_Strings_to_Make_Palindrome.java
+++ Strings/Split_Strings_to_Make_Palindrome.java
@@ -1,3 +1,32 @@
+//Brute Approach 1
+class Solution {
+    public boolean checkPalindromeFormation(String a, String b) {
+        if(a.length()==1 && b.length()==1){
+            return true;
+        }
+        for(int i=0;i<a.length();i++){
+            String temp1="";
+            String temp2="";
+           String ap=a.substring(0,i);
+           String as=a.substring(i);
+           String bp=b.substring(0,i);
+           String bs=b.substring(i);
+String t=ap+bs;
+String t2=bp+as;
+StringBuilder ta=new StringBuilder(t);
+StringBuilder tb=new StringBuilder(t2);
+if(t.equals(ta.reverse().toString())){
+    return true;
+}
+if(t2.equals(tb.reverse().toString())){
+    return true;
+}
+        }
+        return false;
+    }
+}
+
+//Approach 2
 class Solution {
    public static boolean checkPalindromeFormation(String a, String b) {
         return check(a, b) || check(b, a);
