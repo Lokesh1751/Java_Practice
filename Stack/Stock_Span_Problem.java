91e4833a367f71ef92b5e248573421d806aa395f
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-09-20 19:02:24 +0530

    Create Stock_Span_Problem.java

---
+++ Stack/Stock_Span_Problem.java
@@ -0,0 +1,25 @@
+class Solution
+{
+    //Function to calculate the span of stockâ€™s price for all n days.
+    public static int[] calculateSpan(int price[], int n)
+    {
+        int arr[]=new int[n];
+        Stack<Integer> s=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!s.isEmpty() && price[s.peek()]<=price[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                arr[i]=i+1;
+            }
+            else{
+                arr[i]=i-s.peek();
+            }
+            s.push(i);
+        }
+        
+        return arr;
+    }
+    
+}
+/LEETCODE
