db897a62d7c8d82c728a616f2fdb51e1bf69e8b5
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-10-04 11:54:43 +0530

    Update Copy_List_With_Random_Pointer.java

--- Linked_List/Copy_List_With_Random_Pointer.java
+++ Linked_List/Copy_List_With_Random_Pointer.java
@@ -32,3 +32,47 @@ class Solution {
             return map.get(head);
     }
 }
+
+//APPROACH 2
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head==null){
+            return head;
+        }
+        if(head.random==null && head.next==null){
+            Node newnode=head;
+            newnode.next=null;
+            newnode.random=null;
+        }
+         if(head.random!=null && head.next==null){
+            Node newnode=head;
+            newnode.next=null;
+            newnode.random=head;
+        }
+        Node curr=head;
+        while(curr!=null){
+            Node temp=curr.next;
+            curr.next=new Node(curr.val);
+            curr.next.next=temp;
+            curr=temp;
+        }
+        curr=head;
+        while(curr!=null){
+            if(curr.next!=null){
+                curr.next.random= (curr.random!=null)? curr.random.next:null;
+            }
+            curr=curr.next.next;
+        }
+        Node org=head,copy=head.next;
+        Node temp=copy;
+        while(org!=null){
+            org.next=org.next.next;
+            if(copy!=null && copy.next!=null){
+                copy.next=copy.next.next;
+            }
+            org=org.next;
+            copy=copy.next;
+        }
+        return temp;
+    }
+}
