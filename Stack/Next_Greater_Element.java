53d5935f041f8fad5caf6b28fa33eae7bd4b83fa
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-10-12 13:13:34 +0530

    Update Next_Greater_Element.java

--- Stack/Next_Greater_Element.java
+++ Stack/Next_Greater_Element.java
@@ -35,3 +35,29 @@ a[k++]=i;
 
     }
 }
+
+//Using Stack Approach
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int ans[]=new int[nums1.length];
+        Stack<Integer> st=new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num:nums2){
+            while(!st.isEmpty() && st.peek()<num){
+                map.put(st.pop(),num);
+            }
+            st.push(num);
+        }
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i])){
+                nums1[i]=map.get(nums1[i]);
+            }
+            else{
+                nums1[i]=-1;
+            }
+        }
+        return nums1;
+    }
+}
+//LEETCODE
