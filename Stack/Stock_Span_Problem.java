3c020691602a284593823879cf299fdb1a612645
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-10-17 19:39:08 +0530

    Update Stock_Span_Problem.java

--- Stack/Stock_Span_Problem.java
+++ Stack/Stock_Span_Problem.java
@@ -1,3 +1,36 @@
+//BRUTE FORCE APPROACH
+class Solution
+{
+     public static int finddis(int arr[],int o){
+        int cnt=1;
+        int ele=arr[o];
+     for(int i=o-1;i>=0;i--){
+if(arr[i]>ele){
+    return cnt;
+}
+cnt++;
+     }
+     return cnt;
+    }
+    public static int[] calculateSpan(int arr[], int n)
+    {
+        ArrayList<Integer> l=new ArrayList<>();
+        l.add(1);
+        for(int i=1;i<arr.length;i++){
+           int ele= finddis(arr, i);
+         l.add(ele);
+        }
+        int ans[]=new int[n];
+        int k=0;
+        for(Integer i:l){
+            ans[k++]=i;
+        }
+        return ans;
+    }
+    
+}
+
+//APPROACH 2
 class Solution
 {
     //Function to calculate the span of stockâ€™s price for all n days.
