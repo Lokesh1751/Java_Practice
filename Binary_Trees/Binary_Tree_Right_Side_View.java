90611fbe6f345328552f8610fb3f458b68d76ad1
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-11-02 12:50:55 +0530

    Update Binary_Tree_Right_Side_View.java

--- Binary_Trees/Binary_Tree_Right_Side_View.java
+++ Binary_Trees/Binary_Tree_Right_Side_View.java
@@ -53,4 +53,52 @@ class Solution {
         return sol;
     }
 }
+
+//one more solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     public void printLeftView(TreeNode root, ArrayList<TreeNode> ans, int level) {
+        if (root == null) {
+            return;
+        }
+
+        // Check if the current level is being visited for the first time
+        if (level == ans.size()) {
+            ans.add(root);
+        }
+
+        // Recursively traverse the left and right subtrees
+        printLeftView(root.right, ans, level + 1);
+         printLeftView(root.left, ans, level + 1);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<TreeNode> ans = new ArrayList<>();
+
+        // Perform DFS to update the left view nodes
+        printLeftView(root, ans, 0);
+
+        // Convert the nodes to a list of integers
+        List<Integer> sol = new ArrayList<>();
+        for (TreeNode node : ans) {
+            sol.add(node.val);
+        }
+
+        return sol;
+    }
+}
 //LEETCODE
