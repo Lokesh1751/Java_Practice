2b1e144ca5de932b9dda704ab88356afea5bf47d
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-11-06 12:33:11 +0530

    Create Convert_Binary_Tree_to_Doubly_Linked_List.java

---
+++ Binary_Trees/Convert_Binary_Tree_to_Doubly_Linked_List.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    Node prev=null;
+    Node head=null;
+    //Function to convert binary tree to doubly linked list and return it.
+    Node bToDLL(Node root)
+    {
+	if(root==null){
+	    return null;
+	}
+	bToDLL(root.left);
+	if(prev==null){
+	    head=root;
+	}
+	else{
+	    root.left=prev;
+	    prev.right=root;
+	}
+	prev=root;
+	bToDLL(root.right);
+	return head;
+    }
+}
+//GFG
