8aed0668fb40d4c20ebbc4ce99e8b10fa5596e9a
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-11-27 15:31:13 +0530

    Update Balanced_binary_tree.java

--- Binary_Trees/Balanced_binary_tree.java
+++ Binary_Trees/Balanced_binary_tree.java
@@ -34,4 +34,42 @@ class Solution {
         return false;
     }
 }
+
+///APPROACH 2 O(N)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=height(root.left);
+        int right=height(root.right);
+        int bf=Math.abs(left-right);
+        if(bf>1 || left==-1 || right==-1){
+            return -1;
+        }
+        return 1+Math.max(left,right);
+
+    }
+    public boolean isBalanced(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return height(root)!=-1;
+    }
+}
 //LEETCODE
