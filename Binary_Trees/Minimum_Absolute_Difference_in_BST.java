9704c0ae7e3bd7f2c37626511b06a5a60623f9f4
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-11-28 12:48:58 +0530

    Update Minimum_Absolute_Difference_in_BST.java

--- Binary_Trees/Minimum_Absolute_Difference_in_BST.java
+++ Binary_Trees/Minimum_Absolute_Difference_in_BST.java
@@ -42,4 +42,41 @@ class Solution {
        return mini;
     }
 }
+
+
+//OPTIMIZE SOLUTION
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   public int mindiff=Integer.MAX_VALUE;
+   public int prev=-1;
+    public int getMinimumDifference(TreeNode root) {
+        if(root==null){
+            return mindiff;
+        }
+        getMinimumDifference(root.left);
+        if(prev!=-1){
+            mindiff=Math.min(mindiff,root.val-prev);
+        }
+        if(root!=null){
+            prev=root.val;
+        }
+        getMinimumDifference(root.right);
+        return mindiff;
+    }
+}
+
 //LEETCODE
