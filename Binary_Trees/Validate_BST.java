e79189e75d4cce2ef51ced916bfec589d08b1188
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-11-29 11:19:52 +0530

    Update Validate_BST.java

--- Binary_Trees/Validate_BST.java
+++ Binary_Trees/Validate_BST.java
@@ -45,4 +45,39 @@ class Solution {
        return false;
     }
 }
+
+//OPTIMIZE
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     boolean helper(TreeNode node , Integer low , Integer high){
+         if(node==null){
+             return true;
+         }
+         if(high!=null && node.val>=high){
+             return false;
+         }
+         if(low!=null && node.val<=low){
+             return false;
+         }
+         return helper(node.left,low,node.val) && helper(node.right,node.val,high);
+     }
+    public boolean isValidBST(TreeNode root) {
+         return  helper(root,null,null);
+    }
+}
+
 //LEETCODE
