e77ec17e8c2eaa190ae2e73ebe16fe43037c304d
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-11-30 13:13:55 +0530

    Update Closest_Nodes_Queries_in_BST.java

--- Binary_Trees/Closest_Nodes_Queries_in_BST.java
+++ Binary_Trees/Closest_Nodes_Queries_in_BST.java
@@ -83,4 +83,103 @@
         return sol;
     }
 }
+
+//OPTIMIZED SOLUTION
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Solution {
+    private static int findJustSmaller(List<Integer> arrayList, int targetValue) {
+        int justSmaller = Integer.MIN_VALUE;
+
+        for (int num : arrayList) {
+            if (num < targetValue && num > justSmaller) {
+                justSmaller = num;
+            }
+        }
+
+        return justSmaller;
+    }
+
+    private static int findJustGreater(List<Integer> arrayList, int targetValue) {
+        int justGreater = Integer.MAX_VALUE;
+
+        for (int num : arrayList) {
+            if (num > targetValue && num < justGreater) {
+                justGreater = num;
+            }
+        }
+
+        return justGreater;
+    }
+
+    public List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        sol(root);
+        return ans;
+    }
+
+    public void sol(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        sol(root.left);
+        ans.add(root.val);
+        sol(root.right);
+    }
+
+    public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
+        List<Integer> v = inorderTraversal(root);
+        boolean check;
+
+
+       List<List<Integer>> ans = new ArrayList<>();
+
+        for (int x : queries) {
+            int num1 = -1, num2 = -1;
+            int first = 0, last = v.size() - 1;
+
+            while (last >= first) {
+                int mid = (first + last) / 2;
+                if (v.get(mid) <= x) {
+                    num1 = v.get(mid);
+                    first = mid + 1;
+                } else {
+                    last = mid - 1;
+                }
+            }
+
+            first = 0;
+            last = v.size() - 1;
+
+            while (last >= first) {
+                int mid = (first + last) / 2;
+                if (v.get(mid) >= x) {
+                    num2 = v.get(mid);
+                    last = mid - 1;
+                } else {
+                    first = mid + 1;
+                }
+            }
+
+            ans.add(Arrays.asList(num1, num2));
+        }
+
+        return ans;
+    }
+}
+
 //leetcode
