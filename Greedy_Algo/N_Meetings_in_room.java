b38964490abf5934d5e5080da9f976f7b326494e
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-02-16 11:55:17 +0530

    Create N_Meetings_in_room.java

---
+++ Greedy_Algo/N_Meetings_in_room.java
@@ -0,0 +1,34 @@
+
+class Solution 
+{
+    static class pair{
+        int s;
+        int e;
+        pair(int s,int e){
+            this.s=s;
+            this.e=e;
+        }
+    }
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    public static int maxMeetings(int start[], int end[], int n)
+    {
+        pair arr[]=new pair[n];
+        for(int i=0;i<n;i++){
+            arr[i]=new pair(start[i],end[i]);
+        }
+         Arrays.sort(arr, (a, b) -> a.e - b.e);
+        int startidx=arr[0].s;
+        int endidx=arr[0].e;
+        int count=1;
+        for(int i=1;i<arr.length;i++){
+            if(endidx<arr[i].s){
+                count++;
+                endidx=arr[i].e;
+            }
+        }
+        return count;
+       
+    }
+}
+//GFG
