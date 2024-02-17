9916d5cb794e472f5e039964830367ed384d56d6
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-02-17 18:56:45 +0530

    Create Fibbonacci.java

---
+++ Dynamic_Programming/Fibbonacci.java
@@ -0,0 +1,36 @@
+//Using tabulation method bottom up approach
+class Solution {
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+         int dparray[]=new int[n+1];
+      dparray[0]=0;
+      dparray[1]=1;
+      for(int i=2;i<=n;i++){
+          dparray[i]=dparray[i-1]+dparray[i-2];
+      }
+      return dparray[n];
+    }
+}
+
+//using recursion+memorization top down approach
+class Solution {
+     public static int helper(int n,int dp[]){
+        if(n==0 || n==1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
+    }
+    public int fib(int n) {
+         int dparray[]=new int[n+1];
+       Arrays.fill(dparray,-1);
+       return helper(n,dparray);
+    }
+}
+
+//LEETCODE
