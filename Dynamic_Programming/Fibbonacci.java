8da92107d7567178473084cf350423389057860c
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-02-17 19:01:37 +0530

    Update Fibbonacci.java

--- Dynamic_Programming/Fibbonacci.java
+++ Dynamic_Programming/Fibbonacci.java
@@ -32,5 +32,20 @@ class Solution {
        return helper(n,dparray);
     }
 }
-
+//using space optimization
+class Solution {
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+       int prev1=1;
+       int prev2=0;
+       for(int i=2;i<=n;i++){
+           int curr=prev1+prev2;
+           prev2=prev1;
+           prev1=curr;
+       }
+       return prev1;
+    }
+}
 //LEETCODE
