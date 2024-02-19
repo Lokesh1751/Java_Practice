eefe6c84dcf4931b88a711bbff2870ce4a72541b
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-02-19 13:41:47 +0530

    Update Maximum_sum_of_non_adjacent_elements.java

--- Dynamic_Programming/Maximum_sum_of_non_adjacent_elements.java
+++ Dynamic_Programming/Maximum_sum_of_non_adjacent_elements.java
@@ -35,6 +35,20 @@ public class Solution {
 		}
 		return dp[nums.size()-1];
 	}
+
+	public static int spaceoptimization(ArrayList<Integer> nums){
+		int n=nums.size();
+		int prev2=0;
+		int prev1=nums.get(0);
+		for(int i=1;i<n;i++){
+			int inc=prev2+nums.get(i);
+			int exc=prev1+0;
+			int ans=Math.max(inc,exc);
+			prev2=prev1;
+			prev1=ans;
+		}
+		return prev1;
+	}
 	public static int maximumNonAdjacentSum(ArrayList<Integer> nums) {
 		int dp[]=new int[nums.size()+1];
 		Arrays.fill(dp,-1);
