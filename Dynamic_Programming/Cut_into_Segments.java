4e4068101e4b8a1786287bbcc5d93ad966c19ee3
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-02-19 15:04:31 +0530

    Update Cut_into_Segments.java

--- Dynamic_Programming/Cut_into_Segments.java
+++ Dynamic_Programming/Cut_into_Segments.java
@@ -29,6 +29,27 @@ public class Solution {
         dp[n]= Math.max(ans1,Math.max(ans2, ans3));
         return dp[n];
     }
+
+     public static int solvetab(int n,int x,int y,int z){
+         int dp[]=new int[n+1];
+        Arrays.fill(dp,Integer.MIN_VALUE);
+        dp[0]=0;
+
+        for(int i=1;i<=n;i++){
+            if(i-x>=0){
+                dp[i]=Math.max(dp[i],dp[i-x]+1);
+            }
+            if(i-y>=0){
+                dp[i]=Math.max(dp[i],dp[i-y]+1);
+            }
+            if(i-z>=0){
+                dp[i]=Math.max(dp[i],dp[i-z]+1);
+            }
+        }
+        if(dp[n]<0)
+        return 0;
+        return dp[n];
+    }
     public static int cutSegments(int n, int x, int y, int z) {
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
