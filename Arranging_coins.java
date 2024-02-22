df16c346680adf998a165b05d1ef9c82a70594be
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-02-22 23:38:49 +0530

    Update Arranging_coins.java

--- Arranging_coins.java
+++ Arranging_coins.java
@@ -16,4 +16,28 @@ else{
         return cnt;
     }
 }
+
+//Using Binary search
+class Solution {
+    public int arrangeCoins(int n) {
+        long start=0;
+        long end=n;
+        long k,curr;
+        while(start<=end){
+             k=start+(end-start)/2;
+             curr=k*(k+1)/2;
+            if(curr==n){
+                return (int)k;
+            }
+            if(curr>n){
+                end=k-1;
+            }
+            else{
+                start=k+1;
+            }
+        }
+        return (int)end;
+    }
+}
+
 //LEETCODE
