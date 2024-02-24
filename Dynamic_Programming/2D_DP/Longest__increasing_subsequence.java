8a6689fb27fb110637b02f5caf18ffd4fa0c680e
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-02-24 17:55:37 +0530

    Update Longest__increasing_subsequence.java

--- Dynamic_Programming/2D_DP/Longest__increasing_subsequence.java
+++ Dynamic_Programming/2D_DP/Longest__increasing_subsequence.java
@@ -83,4 +83,35 @@ public static int helperSO(int arr[]){
         return helpertab(nums);
     }
 }
+
+//Most Optimal Approach 
+class Solution {
+    public int helper(int[] nums){
+        if(nums.length==0){
+            return 0;
+        }
+        ArrayList<Integer> al=new ArrayList<>();
+        al.add(nums[0]);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>al.get(al.size()-1)){
+                al.add(nums[i]);
+            }
+            else{
+                int idx = al.size() - 1;
+    while (idx >= 0 && nums[i] <=al.get(idx)) {
+        idx--;
+    }
+    if (idx + 1 < al.size()) {
+        al.set(idx + 1, nums[i]);
+    } else {
+        al.add(nums[i]);
+    }
+            }
+        }
+        return al.size();
+    }
+    public int lengthOfLIS(int[] nums) {
+        return helper(nums);
+    }
+}
 //LEETCODE
