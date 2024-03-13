1e8d62e79db16c7eb1ff773cea633191ecdf6c6d
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-03-13 12:28:04 +0530

    Update Maximum_Number_of_Balloons.java

--- Maximum_Number_of_Balloons.java
+++ Maximum_Number_of_Balloons.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int arr[]=new int[26];
+        Arrays.fill(arr,0);
+        for(Character c:text.toCharArray()){
+            int l=c-'a';
+            arr[l]++;
+        }
+        String req="balloon";
+        int ans=Integer.MAX_VALUE;
+        for(Character c:req.toCharArray()){
+            if(c=='l' || c=='o'){
+ans=Math.min(ans,arr[c-'a']/2);
+            }
+            else{
+                ans=Math.min(ans,arr[c-'a']);
+            }
+        }
+        return ans;
+    }
+}
+//leetcode
