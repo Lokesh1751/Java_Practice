1724068749687bc1cd44815b45851256e5ee3266
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2024-03-19 14:10:29 +0530

    Update Subarray_sum_equals_k.java

--- Subarray_sum_equals_k.java
+++ Subarray_sum_equals_k.java
@@ -1,3 +1,23 @@
+//Brute force
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int sum=nums[i];
+            if(sum==k){
+                count++;
+            }
+            for(int j=i+1;j<nums.length;j++){
+    sum+=nums[j];
+if(sum==k){
+    count++;
+}
+            }
+        }
+        return count;
+    }
+}
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
         HashMap<Integer,Integer> hm=new HashMap<>();
